2cdac15887f327936226cddcdf40eab15229572a
Oliver Lorton <oliverlorton@gmail.com>
2022-07-17 17:38:49 +0100

    A starting point for polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,9 +6,12 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+# # Example to launch two bars: bar1 and bar2
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "---" >> /tmp/polybar-main.log
+polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
 
 echo "Bars launched..."
