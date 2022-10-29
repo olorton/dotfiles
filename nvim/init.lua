f1d24258b8d113fc569ceeb9d8a8bb9f29f94dc4
Oliver Lorton <oliverlorton@gmail.com>
2022-10-29 18:31:13 +0100

    Remove pluging autoload script

--- nvim/init.lua
+++ nvim/init.lua
@@ -138,14 +138,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd([[packadd packer.nvim]])
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
