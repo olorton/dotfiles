128802f9e8aa822660488b32c19fc3cfe6d69a1a
Oliver Lorton <oliverlorton@gmail.com>
2022-10-29 19:32:33 +0100

    Add Comment plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -43,7 +43,12 @@ return packer.startup(function(use)
   use { "wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- Have packer manage itself
   --use { "nvim-lua/plenary.nvim", commit = "4b7e52044bbb84242158d977a50c4cbcd85070c7" } -- Useful lua functions used by lots of plugins
   --use { "windwp/nvim-autopairs", commit = "4fc96c8f3df89b6d23e5092d31c866c53a346347" } -- Autopairs, integrates with both cmp and treesitter
-  --use { "numToStr/Comment.nvim", commit = "97a188a98b5a3a6f9b1b850799ac078faa17ab67" }
+  use {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end
+  }
   --use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" }
   --use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
   --use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
@@ -57,9 +62,9 @@ return packer.startup(function(use)
   --use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
   --use {"folke/which-key.nvim"}
   use {
-    'numToStr/Navigator.nvim',
+    "numToStr/Navigator.nvim",
     config = function()
-        require('Navigator').setup()
+      require("Navigator").setup()
     end
   }
 
