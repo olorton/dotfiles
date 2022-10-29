daaf46a3aa1b627f1cf9130d8fa2e6e3956e3d72
Oliver Lorton <oliverlorton@gmail.com>
2022-10-29 18:51:48 +0100

    Rename var

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,10 +1,10 @@
-local key_opts= { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", key_opts)
+keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -18,46 +18,46 @@ vim.g.maplocalleader = " "
 
 -- Normal ----------------------------
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", key_opts)
-keymap("n", "<C-j>", "<C-w>j", key_opts)
-keymap("n", "<C-k>", "<C-w>k", key_opts)
-keymap("n", "<C-l>", "<C-w>l", key_opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- File explorer
-keymap("n", "<leader>e", ":Lex 30<cr>", key_opts)
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", key_opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", key_opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", key_opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", key_opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", key_opts)
-keymap("n", "<S-h>", ":bprevious<CR>", key_opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", key_opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", key_opts)
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode 
---keymap("i", "jk", "<ESC>", key_opts)
---keymap("i", "kj", "<ESC>", key_opts)
+--keymap("i", "jk", "<ESC>", opts)
+--keymap("i", "kj", "<ESC>", opts)
 
 -- Visual ----------------------------
 -- Stay in indent mode
-keymap("v", "<", "<gv", key_opts)
-keymap("v", ">", ">gv", key_opts)
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", key_opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", key_opts)
-keymap("v", "p", '"_dP', key_opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
 
 -- Visual Block ----------------------
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", key_opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", key_opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", key_opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", key_opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
