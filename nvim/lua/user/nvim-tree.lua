26654cdb378b3c2fee787cf362b9c8cf7a1b2265
Oliver Lorton <oliverlorton@gmail.com>
2022-12-21 14:48:19 +0000

    Show files/dirs excluded by git

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -15,6 +15,10 @@ nvim_tree.setup {
     enable = true,
     update_cwd = true,
   },
+  git = {
+    enable = true,
+    ignore = false,
+  },
   renderer = {
     root_folder_modifier = ":t",
     icons = {
