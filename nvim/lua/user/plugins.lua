9be7e7a483eb98c568e809aebae50b4f7fdd4e57
Oliver Lorton <oliverlorton@gmail.com>
2023-02-19 20:00:31 +0000

    Revert "Get rid of bufferline and tabline"
    This reverts commit b7c2333fbfc68a86c13c3ed3426a6812005c4b61.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -47,6 +47,7 @@ return packer.startup(function(use)
   use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" }
   -- use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
   use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
+  use { "akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
 	use { "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" }
   use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
   use { "akinsho/toggleterm.nvim", commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda" }
