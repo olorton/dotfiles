b20fd05277dfa49b6fff5d03ffa61c08c7d9451d
Oliver Lorton <oliverlorton@gmail.com>
2023-02-19 20:01:53 +0000

    Revert "Fix window/pane navigation between nvim and tmux"
    This reverts commit c8a49b8bbac851a5b7ed90a9be1496237b15dc60.

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -19,12 +19,11 @@ vim.g.maplocalleader = " "
 --   command_mode = "c",
 
 -- Normal --
--- Window navigation that works with Tmux
-keymap("n", "<C-h>", "<CMD>NavigatorLeft<CR>", opts)
-keymap("n", "<C-l>", "<CMD>NavigatorRight<CR>", opts)
-keymap("n", "<C-k>", "<CMD>NavigatorUp<CR>", opts)
-keymap("n", "<C-j>", "<CMD>NavigatorDown<CR>", opts)
--- vim.keymap.set("n", "<C-p>", "<CMD>NavigatorPrevious<CR>", opts)
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -91,14 +91,6 @@ return packer.startup(function(use)
 	-- Git
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
 
-  -- Navigator
-  use {
-    'numToStr/Navigator.nvim',
-    config = function()
-        require('Navigator').setup()
-    end
-}
-
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
