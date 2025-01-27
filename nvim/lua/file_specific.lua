5f261a5f44056af009d15813d1d0f6590f185e29
Oliver Lorton <oliverlorton@gmail.com>
2025-01-27 11:16:40 +0000

    Temp disable problemattic bits of new setup

--- nvim/lua/file_specific.lua
+++ nvim/lua/file_specific.lua
@@ -1,44 +1,65 @@
--------------------------------------------------------------------------------
--- Python
+-- -------------------------------------------------------------------------------
+-- -- Python
 
--- Autocommands for requirements.txt files
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    pattern = "*requirements.txt",
-    callback = function()
-        vim.bo.filetype = "text"
-        vim.bo.spell = false
-    end,
-})
+-- -- Autocommands for requirements.txt files
+-- vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+--     pattern = "*requirements.txt",
+--     callback = function()
+--         vim.bo.filetype = "text"
+--         vim.bo.spell = false
+--     end,
+-- })
 
--- Autocommands for Python file type
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "python",
-    callback = function()
-        vim.bo.omnifunc = "pythoncomplete#Complete"
-        vim.bo.define = "^\\s*\\(def\\|class\\)"
-        vim.wo.foldnestmax = 1
-    end,
-})
+-- -- Autocommands for Python file type
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "python",
+--     callback = function()
+--         vim.bo.omnifunc = "pythoncomplete#Complete"
+--         vim.bo.define = "^\\s*\\(def\\|class\\)"
+--         vim.wo.foldnestmax = 1
+--     end,
+-- })
 
--- Enable enhanced syntax highlighting for Python
-vim.g.python_highlight_all = 1
+-- -- Enable enhanced syntax highlighting for Python
+-- vim.g.python_highlight_all = 1
 
--------------------------------------------------------------------------------
--- Golang
+-- -------------------------------------------------------------------------------
+-- -- Golang
 
--- Hide tab chars when writing go files.
--- TODO `nolist` hides all list chars including non-breaking spaces and 
--- trailing spaces, consider reworking this so that it only affects tab chars.
-vim.cmd [[
-au FileType go setlocal nolist
-]]
+-- -- Hide tab chars when writing go files.
+-- -- TODO `nolist` hides all list chars including non-breaking spaces and
+-- -- trailing spaces, consider reworking this so that it only affects tab chars.
+-- -- vim.opt.listchars = {
+-- --     tab = '▸ ',
+-- --     trail = '·',
+-- --     eol = '↲',
+-- --     space = '·'
+-- -- }
 
--------------------------------------------------------------------------------
--- PHP
+-- -- vim.cmd [[
+-- -- au FileType go setlocal nolist
+-- -- ]]
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "php" },
-    callback = function()
-        vim.bo.commentstring = "// %s"
-    end,
-})
+-- -- vim.api.nvim_create_autocmd("FileType", {
+-- --   pattern = "go",
+-- --   callback = function()
+-- --     vim.opt_local.listchars = {
+-- --       tab = '▸ ',
+-- --       trail = '·',
+-- --       eol = '↲',
+-- --       space = '·'
+-- --     }
+-- --     vim.opt_local.list = true
+-- --   end
+-- -- })
+
+-- -------------------------------------------------------------------------------
+-- -- PHP
+
+-- -- Instruct vim-commentary plugin to use `//` to toggle comments
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = { "php" },
+--     callback = function()
+--         vim.bo.commentstring = "// %s"
+--     end,
+-- })
