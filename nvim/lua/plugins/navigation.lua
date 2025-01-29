3fd81f4828b0d2ce99c9ca0906fa0868cd4ff152
Oliver Lorton <oliverlorton@gmail.com>
2025-01-29 06:30:30 +0000

    Replace fzf with Telescope

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -2,6 +2,20 @@ return {
     "christoomey/vim-tmux-navigator",
     "scrooloose/nerdtree",
     "bling/vim-bufferline",
-    { "junegunn/fzf", build = "./install --all" },
-    "junegunn/fzf.vim",
+    -- "bling/vim-bufferline",
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        pickers = {
+            buffers = {
+                initial_mode = "normal", -- Override for buffers picker
+            },
+        },
+        keys = {
+            { "<C-p>", "<cmd>Telescope find_files<CR>", mode = "n" },
+            { "<C-g>", "<cmd>Telescope live_grep<CR>", mode = "n" },
+            { ";", "<cmd>Telescop buffers<CR>", mode = "n" },
+        },
+    },
 }
