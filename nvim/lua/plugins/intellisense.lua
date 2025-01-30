f4fee62f3f39421f025a50ad3312b4e3258b55b6
Oliver Lorton <oliverlorton@gmail.com>
2025-01-30 10:59:26 +0000

    Add keymap to see the full diagnostic, rather than just in-line

--- nvim/lua/plugins/intellisense.lua
+++ nvim/lua/plugins/intellisense.lua
@@ -130,6 +130,8 @@ return {
                     vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
                     vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
                     vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+                    vim.keymap.set('n', '<leader>E', '<cmd>lua vim.diagnostic.open_float()<CR>',
+                        { noremap = true, silent = true })
                 end,
             })
 
